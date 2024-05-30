85432e67ccdc864862eaf23970dacb93b6c054fc
Pritaasheniy Ananthan <applemapple100@gmail.com>
2024-05-30 12:43:55 +0800

    Added a folder.

--- customisation/myproject/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import CoffeeOption
-
-def home(request):
-    options = CoffeeOption.objects.all()
-    return render(request, 'myproject/home.html', {'options': options})
